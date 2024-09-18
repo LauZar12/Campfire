25203b002bc6161a6d91b21ae165d5052f18f4ec
MiguelCock <macockc@eafit.edu.co>
2024-09-18 15:03:23 -0500

    connected all the simple views

--- shop/views.py
+++ shop/views.py
@@ -14,23 +14,37 @@ class Home(View):
 class Login(View):
     template_name = "login.html"
 
+    def get(self, request):
+        return render(request, self.template_name, {})
+
 
 # =========================SIGNUP=========================
 class Signup(View):
     template_name = "signup.html"
+
+    def get(self, request):
+        return render(request, self.template_name, {})
 
 
 # =========================GAMES=========================
 class Games(View):
     template_name = "games.html"
 
+    def get(self, request):
+        return render(request, self.template_name, {})
+
 
 # =========================GAME=========================
 class Game(View):
     template_name = "game.html"
+
+    def get(self, request):
+        return render(request, self.template_name, {})
 
 
 # =========================CART=========================
 class Cart(View):
     template_name = "cart.html"
 
+    def get(self, request):
+        return render(request, self.template_name, {})
