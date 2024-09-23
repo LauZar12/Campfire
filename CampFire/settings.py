f413bcece594e95772c533a9c1599529581391ba
MiguelCock <macockc@eafit.edu.co>
2024-09-23 16:42:13 -0500

    created the models to implement the forms

--- CampFire/settings.py
+++ CampFire/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
 ]
 
 MIDDLEWARE = [
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# ========== USER ==========
+class Costumer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    credit_card = models.CharField(max_length=255)
+
+
+# ========== GAME ==========
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10)
+    description = models.TextField()
+
+
+class Stock(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='stock')
+    quantity = models.PositiveIntegerField(default=1)
+
+
+# ========== CART ==========
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shopping_carts')
+
+
+class CartItem(models.Model):
+    shopping_cart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE, related_name='cart_items')
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveIntegerField(default=1)
+
+
+# ========== CATEGORIES ==========
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    desciption = models.CharField(max_length=255)
+
+
+class GameCategories(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='game_categories')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='game_categories')
+
+
+# ========== USER/GAME INTERACTIONS =========
+class GameOwner(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='game_owner')
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='game_owner')
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review')
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='review')
