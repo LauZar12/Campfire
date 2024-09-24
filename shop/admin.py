12bcbf1676fa3b61c0e513f81150c12ed6bd4cfb
MiguelCock <macockc@eafit.edu.co>
2024-09-23 23:25:13 -0500

    added some videogames and change the view to display them

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Game, Category, Stock
+from .models import Game, Category, Stock, GameCategories
 
 
 # Register your models here
 admin.site.register(Game)
+admin.site.register(GameCategories)
 admin.site.register(Category)
 admin.site.register(Stock)
--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import Home, Login, Signup, Games, Game, Cart, Account
+from .views import Home, Login, Logout, Signup, Games, GameView, Cart, Account
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('login/', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
     path('signup/', Signup.as_view(), name='signup'),
     path('games/', Games.as_view(), name='games'),
-    path('game/', Game.as_view(), name='game'),
+    path('game/', GameView.as_view(), name='game'),
     path('cart/', Cart.as_view(), name='cart'),
     path('account/', Account.as_view(), name='account'),
 ]
