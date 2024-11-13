71124e47fab4b5d1ec1530434508f8904675bacc
Moisés Arrieta <moisesdavidarh@gmail.com>
2024-11-12 20:49:11 -0500

    start api of other grup

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,6 @@
 # Miguel Angel Cock Cano
 from django.urls import path
-from .views import Home, Login, Logout, Signup, Games, GameView, Cart, Account, GamesRest, twitchAPI
+from .views import Home, Login, Logout, Signup, Games, GameView, Cart, Account, GamesRest, twitchAPI  #productos_aliados
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
@@ -13,5 +13,6 @@ urlpatterns = [
     path('account/', Account.as_view(), name='account'),
 
     path('twitchAPI/', twitchAPI.as_view(), name='twitchAPI'),
-    path('gamesAPI/', GamesRest.as_view(), name='')
+    path('gamesAPI/', GamesRest.as_view(), name=''),
+    #path('productos-aliados/', productos_aliados.as_view(), name='productos_aliados')
 ]
